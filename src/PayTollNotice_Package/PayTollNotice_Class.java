fd106f7a553c748939c71a58679b54d094012ad6
FATIMAT <FATIMAT@ATNSW-BENCH012.dialoggroup.internal>
2016-05-05 15:07:30 +1000

    automatic

--- src/PayTollNotice_Package/PayTollNotice_Class.java
+++ src/PayTollNotice_Package/PayTollNotice_Class.java
@@ -43,6 +43,7 @@ public class PayTollNotice_Class extends PerfDB {
   @Test
   public void PayTollNotice_Function() throws InterruptedException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 	
+	  System.out.println("automatic builds");
 	  System.out.println("Todays First commit");
 	  System.out.println("my new stmt");
 	  System.out.println("stmt for running jenkins");
